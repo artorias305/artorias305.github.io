14e516ed08d5a770def6dc2c28b48a2d9b681432
Artorias <105161738+artorias305@users.noreply.github.com>
2023-08-25 20:33:17 -0300

    added fade animations when scrolling

---
+++ fade.js
@@ -0,0 +1,30 @@
+// Function to check if an element is in the viewport
+function isInViewport(element, buffer = 100) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= -buffer &&
+      rect.left >= -buffer &&
+      rect.bottom <= (window.innerHeight + buffer || document.documentElement.clientHeight + buffer) &&
+      rect.right <= (window.innerWidth + buffer || document.documentElement.clientWidth + buffer)
+    );
+  }
+  
+  // Function to handle the fade-in and fade-out effect
+  function handleScrollAnimations() {
+    const fadeElements = document.querySelectorAll('.fade-in');
+    
+    fadeElements.forEach(element => {
+      if (isInViewport(element)) {
+        element.classList.add('visible');
+      } else {
+        element.classList.remove('visible');
+      }
+    });
+  }
+  
+  // Attach the handleScrollAnimations function to the scroll event
+  window.addEventListener('scroll', handleScrollAnimations);
+  
+  // Initial check on page load
+  handleScrollAnimations();
+  
